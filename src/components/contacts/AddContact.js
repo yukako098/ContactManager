3cc0bd8e738899bc1a619b586c727068c8f39863
Yukako Sunabe <yukakosunabe@gmail.com>
2018-11-11 11:17:09 -0800

    modified AddContact.js

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -6,13 +6,20 @@ class AddContact extends Component {
         email: '',
         phone: ''
     }
+  
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+  
+  onChange = e => this.setState({ [e.target.name]: e.target.value})
   render() {
       const { name, email, phone } = this.state;
     return (
       <div className='card mb-3'>
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-            <form>
+            <form onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <label htmlFor="name">Name</label>
                   <input 
@@ -20,7 +27,8 @@ class AddContact extends Component {
                     name="name"
                     className="form-control form-control-lg"
                     placeholder="Enter Name..."
-                    value={name}
+                value={name}
+                onChange = {this.onChange}
                   />
                 </div>
                 <div className="form-group">
@@ -30,7 +38,10 @@ class AddContact extends Component {
                     name="email"
                     className="form-control form-control-lg"
                     placeholder="Enter Email..."
-                    value={email}
+                value={email}
+                onChange = {
+                  this.onChange
+                }
                   />
                 </div>
                 <div className="form-group">
@@ -40,7 +51,10 @@ class AddContact extends Component {
                     name="phone"
                     className="form-control form-control-lg"
                     placeholder="Enter Phone..."
-                    value={phone}
+                value={phone}
+                onChange = {
+                  this.onChange
+                }
                   />
                 </div>
                 <input 
